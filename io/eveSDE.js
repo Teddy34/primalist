3150f8c766fc076429fd1e92b7f7e2b7d1c0ccf8
JSicart <jsicart@intercalleurope.com>
2015-09-30 18:19:29 +0200

    add volume

--- io/eveSDE.js
+++ io/eveSDE.js
@@ -3,10 +3,10 @@ var _ = require('lodash');
 
 var connect = function(connectionString) {
   return dbConnector.connect(connectionString);
-}
+};
 
 var getItems = function(itemList) {
-  return dbConnector.sendQueryWhenReady('SELECT t."typeID", t."typeName", t."groupID" FROM "invTypes" AS "t" WHERE "typeID" IN (' +
+  return dbConnector.sendQueryWhenReady('SELECT t."typeID", t."typeName", t."groupID", t."volume" FROM "invTypes" AS "t" WHERE "typeID" IN (' +
                   _.map(itemList, function(elem) {return elem.typeID;}).toString() +
                   ') ORDER BY "typeID"');
 };
