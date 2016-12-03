958eae4843f82c905654bd3a8fabcc46c43ddc6f
JordiTeddy <jorditeddy@gmail.com>
2016-12-03 11:04:33 +0100

    basic shift to lodash/fp

--- app/application.js
+++ app/application.js
@@ -1,36 +1,43 @@
-const {chain, reduce, extend, map, difference, filter, zip, sortBy} = require('lodash');
+const {
+  flow,
+  partial,
+  map,
+  difference,
+  filter,
+  zip,
+  toNumber,
+  sortBy
+} = require('lodash/fp');
 
 const template = require('./UITable');
 const parseZKillboard = require('./parseZKillboard');
 const {getItems} = require('../io/eveSDE');
 
-const mergeToOneObject = (list) => {
- return reduce(list, (memo,value) => {return extend(memo,value);});
-};
+const mergeToOneObject = (arr) => Object.assign({}, ...arr)
 
 const forceTypeNumber = (elem) => {
-  elem.groupID = Number(elem.groupID);
-  elem.typeID = Number(elem.typeID);
+  elem.groupID = toNumber(elem.groupID);
+  elem.typeID = toNumber(elem.typeID);
   return elem;
 };
 
 const filterEveOnlyAndMarket = (elem) => {
-  //return (elem.groupID < 100000);
   return ((elem.groupID < 100000) && (elem.marketGroupID !== null));
 };
 
 const decorateFromSDE = (itemList) => {
   const parse = (eveSDEItems) => {
-    const myItemIDs = map(itemList, 'typeID');
-    const sdeItemIDs = map(eveSDEItems.rows, 'typeID').map((nId) => String(nId));
-    const diff = difference(myItemIDs,sdeItemIDs); // list not found items
+    const myItemIDs = map('typeID', itemList);
+    const sdeItemIDs = map('typeID', eveSDEItems.rows).map((nId) => String(nId));
+    const diff = difference(myItemIDs, sdeItemIDs); // list not found items
+    const myFilteredItemList = filter((item) => (diff.indexOf(item.typeID) === -1), itemList);
 
-    const myFilteredItemList = filter(itemList, (item) => {
-      return (diff.indexOf(item.typeID) === -1);
-    });
-
-
-    return chain(eveSDEItems.rows).zip(myFilteredItemList).map(mergeToOneObject).map(forceTypeNumber).filter(filterEveOnlyAndMarket).sortBy('groupID').value();
+    return flow(
+      zip(myFilteredItemList),
+      map(mergeToOneObject),
+      map(forceTypeNumber),
+      filter(filterEveOnlyAndMarket)
+      )(eveSDEItems.rows);
   }
 
   return Promise.resolve(itemList)
